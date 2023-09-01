09d0cbfe9a0b8921efd9576f86d353e02a65f974
Ульяна <skipina_ulyana24@mail.ru>
2023-09-02 01:26:30 +0300

    (

--- src/main/java/ru/volkova/arrapp/_main/_Main.java
+++ src/main/java/ru/volkova/arrapp/_main/_Main.java
@@ -11,7 +11,7 @@ public class _Main {
         arr1[2] = 50;
         arr1[3] = 7;
         arr1[4] = -8;
-        arr1[5] = 0;
+        arr1[5] = 33;
         arr1[6] = 6;
         arr1[7] = 1;
         arr1[8] = 14;
@@ -21,6 +21,9 @@ public class _Main {
         utility.sumNum (arr1);
         utility.evenNumAmount(arr1);
         utility.oddNumAmount(arr1);
+        utility.primeNumAmount(arr1);
+        utility.diffSumIndex(arr1);
+        utility.zeroAmount(arr1);
     }
 
 
--- src/main/java/ru/volkova/arrapp/util/Utility.java
+++ src/main/java/ru/volkova/arrapp/util/Utility.java
@@ -6,7 +6,7 @@ public class Utility {
         int sum = 0;
         while (index < 10){
             sum = sum + arr1[index];
-            index = index + 1;
+            index ++;
         }
         System.out.println("Сумма всех чисел массива = " + sum);
     }
@@ -17,10 +17,10 @@ public class Utility {
         while (index<10){
             int num = arr1[index];
             if (num % 2 == 0){
-                amount = amount + 1;
-                index = index + 1;
+                amount ++;
+                index ++;
             } else {
-                index=index +1;
+                index ++;
             }
         }
         System.out.println("Количество всех чётных чисел = " + amount);
@@ -32,13 +32,63 @@ public class Utility {
         while (index<10){
             int num = arr1[index];
             if (num % 2 == 0){
-                index = index + 1;
+                index ++;
             } else {
-                amount = amount + 1;
-                index=index +1;
+                amount ++;
+                index ++;
             }
         }
         System.out.println("Количество всех нечётных чисел = " + amount);
+    }
+
+    public void primeNumAmount(int [] arr1){
+        int index = 0;
+        int amount = 0;
+        int i = 2;
+        while (index<10){
+            int num = arr1[index];
+            if (i <= num/2 && num % i == 0){
+                index ++;
+                i ++;
+            } else {
+                amount ++;
+                index ++;
+                i ++;
+            }
+        }
+        System.out.println("Количество всех простых чисел = " + amount);
+    }
+
+    public void diffSumIndex(int [] arr1){
+        int index = 0;
+        int sumEven = 0;
+        int sumOdd = 0;
+        while (index < 10){
+            if(index % 2 == 0){
+                sumEven = sumEven + arr1[index];
+                index ++;
+            } else {
+                sumOdd = sumOdd + arr1[index];
+                index ++;
+            }
+        }
+        int diff = sumEven - sumOdd;
+        System.out.println("Разность сумм чисел с чётными и нечётными индексами равна = " + diff);
+    }
+
+    public void zeroAmount(int [] arr1){
+        int index = 0;
+        int amount = 0;
+        while (index<10){
+            int num = arr1[index];
+            if (num == 0){
+                amount ++;
+                index ++;
+            } else {
+                index ++;
+            }
+        }
+        System.out.println("Количество раз, когда встречается 0 = " + amount);
     }
 
 }
